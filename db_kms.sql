979de3650c1c0d53dfa44b6bbfa80379bff8692b
Azis Dzelani <azisdzelani@gmail.com>
2016-09-21 14:51:40 +0700

    edit database

--- db_kms.sql
+++ db_kms.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 17, 2016 at 10:48 AM
+-- Generation Time: Sep 21, 2016 at 02:50 PM
 -- Server version: 5.6.26
 -- PHP Version: 5.6.12
 
@@ -30,14 +30,19 @@ CREATE TABLE IF NOT EXISTS `tbl_berita` (
   `id_berita` int(11) NOT NULL,
   `id_user` int(3) NOT NULL,
   `id_kategori_berita` int(11) NOT NULL,
-  `slug_berita` varchar(255) NOT NULL,
   `judul` varchar(255) NOT NULL,
   `isi` text NOT NULL,
-  `status_berita` varchar(20) NOT NULL,
-  `jenis_berita` varchar(20) NOT NULL,
   `gambar` varchar(255) NOT NULL,
-  `tanggal` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `tanggal` date NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbl_berita`
+--
+
+INSERT INTO `tbl_berita` (`id_berita`, `id_user`, `id_kategori_berita`, `judul`, `isi`, `gambar`, `tanggal`) VALUES
+(1, 7, 7, 'LAN Kembali Gelar Temu Inovasi Administrasi Negara', '<p>Jakarta &ndash; Lembaga Administrasi Negara (LAN) akan menyelenggarakan Temu Inovasi Administrasi Negara (Inagara), Selasa (27/9) hingga Rabu (28/9) mendatang, di Gedung Makarti Bhakti Nagari, Kampus PPLPN LAN Pejompongan, Jakarta.</p><p>Temu Inovasi Administrasi Negara yang kedua kalinya digelar ini bertujuan untuk semakin menginspirasi para aparatur sipil negara di lingkungan Kementerian/Lembaga/Pemda untuk mengembangkan inovasi-inovasi di bidang pelayanan publik.</p><p>Koordinator acara dari Pusat Promosi Inovasi dan Pengembangan Kapasitas, Sabrina mengatakan, kegiatan Temu Inovasi Administrasi Negara (Inagara) ini akan berlangsung selama dua hari dengan dihadiri sejumlah tokoh yang dinilai memiliki komitmen tinggi dalam mendorong inovasi di wilayahnya.</p><p>&ldquo;Sejumlah Bupati/Walikota dipastikan akan hadir dalam Temu Inagara. Mereka yang hadir juga akan dianugerahi Ingara Award sebagai penghargaan atas komitmennya membuka ruang inovasi di sektor publik dalam pemerintahannya,&rdquo; jelasnya.</p><p>Tidak hanya itu saja, lanjut dia, Temu Inagara ini juga akan diisi dengan talkshow terkait masalah inovasi. Sejumlah<em>best practices</em>&nbsp;inovasi dan laboratorium inovasi yang saat ini sudah berjalan juga akan diperbincangkan untuk mengulas lebih jauh tantangan dan kendala yang dihadapi saat melakukan inovasi sektor publik.</p><p>&ldquo;Lembaga Administrasi Negara juga akan mengundang beberapa narasumber dari Kemenpan &amp; RB serta Kemendagri. Para narasumber itu akan mengulas berbagai hal terkait dengan inovasi di sektor publik,&rdquo; ujarnya.</p><p>Sabrina merinci, tema-tema yang akan diusung antara lain : Deputi Inovasi Administrasi Negara LAN Tri Widodo akan berbicara mengenai Pengembangan Laboratorium Inovasi untuk memperkuat sistem Inagara, Deputi Pelayan Publik Kemenpan dan RB Diah Natalisa dengan tema Gerakan Inovasi Massive dalam Reformasi Birokrasi, serta Sekjen Kementerian Dalam Negeri Yuswandi A Temenggung dengan tema Strategi Kebijakan Akselerasi Inovasi Pemerintah Daerah.</p><p>&ldquo;Acara Temu Inagara yang kedua ini juga akan diisi dengan Penandatanganan Deklarasi Tri Satya Inagara sebagai wujud komitmen bersama para kepala daerah dalam membumikan inovasi di sektor pemerintah,&rdquo; tutupnya. (Humas)</p>', 'jadwal_akademik_smt_ganjil.png', '2016-09-25'),
+(2, 7, 8, 'LAN Serahkan Sertifikat Akreditasi Kepada 18 Lembaga Diklat', '<p>Jakarta &ndash; Sebanyak 18 Lembaga Diklat Pemerintah memperoleh sertifikat akreditasi dari Lembaga Administrasi Negara selaku instansi pembina diklat aparatur. Akreditasi ini dimaksudkan untuk menjaga kualitas mutu dan penyelenggaraan diklat.</p><p>Kepala Lembaga Administrasi Negara (LAN) Dr. Adi Suryanto, Msi mengatakan, akreditasi yang dilakukan oleh LAN terhadap Lembaga Diklat itu dimaksudkan untuk mengukur sejauh mana profesionalisme Lembaga Diklat selaku penyelenggara.</p><p>&ldquo;Ada banyak komponen yang kita asses terkait akreditasi, baik itu dari kelembagaan diklat, tenaga kediklatan, fasilitas diklat, pengelola, tenaga pengajar, serta pengelola sistem informasi diklat. Jadi semua komponen kita nilai untuk menjamin mutunya,&rdquo; jelasnya saat memberikan sambutan dalam acara penyerahan Sertifikat Akreditasi Lembaga Diklat Pemerintah, di Aula Makarti Bhakti Nagari, Gedung A Lantai II, LAN Pusat, Vetaran, Jakarta, Jumat (16/9).</p><p>Kepala LAN mengatakan, profesionalisme Aparatur Sipil Negara (ASN) saat ini menjadi tuntutan yang tidak bisa dihindari. UU ASN juga memiliki filosofi pegawai ASN sebagai aset dan bukan merupakan beban negara. Dengan demikian, profesionalisme dan kompetensinya harus ditingkatkan.</p><p>&ldquo;Perubahan paradigma ASN sebagai asset organisasi menuntut organisasi untuk senantiasa memelihara dan mengembangkan secara terus-menerus aparatur sipil di lingkungannya. Disinilah lembaga diklat pemerintah memiliki peran yang sangat strategis,&rdquo; jelasnya.</p><p>Dalam kesempatan itu, Kepala LAN juga menyinggung PP No. 18 Tahun 2016 tentang Organisasi Perangkat Daerah yang menempatkan lembaga diklat Kab/Kota tidak lagi menjadi lembaga diklat yang mandiri. Dengan posisi ini, Kepala LAN meminta agar Badan Diklat Provinsi memainkan peran lebih besar untuk mengatur pengembangan kompetensi ASN di tingkat Kabupaten/Kota.</p><p>Selain itu, lanjut dia, juga perlu ada sertifikasi pada setiap program diklat yang diselenggarakan oleh lembaga diklat. Dengan demikian ada kesamaan standar kompetensi di setiap daerah.</p><p>&ldquo;Saya menghimbau agar setiap Badan Diklat Kementerian/Lembaga dapat mengambil alih pembinaan untuk program-program diklat yang ada dibawah pengelolaannya,&rdquo; tutupnya. (choky/humas)</p><p>Adapun ke-18 Lembaga Diklat yang memperoleh sertifikat akreditasi antara lain :</p><p>1. Badan Diklat Provinsi Kalimantan Tengah</p><p>2. Badan Diklat Provinsi Sumatera Barat</p><p>3. Badan Diklat Provinsi Sumatera Utara</p><p>4. Badan Diklat Provinsi NTB</p><p>5. Badan Diklat Provinsi Gorontalo</p><p>6. Pusdiklat BPS</p><p>7. Pusdiklat Kementerian Sekretariat Negara</p><p>8. Pusat Pengembangan SDM Aparatur Perhubungan Kementerian Perhubungan</p><p>9. Pusdiklat Industri Kementerian Perindustrian</p><p>10. Pusdiklat Penanggulangan Kebakaran dan Penyelamatan Provinsi DKI Jakarta</p><p>11. Pusdiklat SDM Lingkungan Hidup dan Kehutanan Kementerian Lingkungan Hidup dan Kehutanan</p><p>12. BPSDM Kementerian Dalam Negeri&nbsp;</p><p>13. Pusdikat Pemerintah Provinsi Jambi</p><p>14. Pusdiklat Kepemimpinan Magelang Kementerian Keuangan</p><p>15. Pusdiklat Pengembangan SDM Kementerian Keuangan</p><p>16. Pusdiklat Manajemen dan Kepemimpinan Mahkamah Agung</p><p>17. Badan Pengembangan dan Pemberdayaan Sumber Daya Manusia Kesehatan Kementerian Kesehatan</p><p>18. Pusdiklat Kementerian Riset Teknologi dan Pendidikan Tinggi&nbsp;</p>', 'jadwal_akademik_smt_ganjil1.png', '2016-09-16');
 
 -- --------------------------------------------------------
 
@@ -77,18 +82,22 @@ CREATE TABLE IF NOT EXISTS `tbl_dokumen` (
   `judul` varchar(50) NOT NULL,
   `keterangan` text NOT NULL,
   `tanggal` date NOT NULL,
-  `file` varchar(50) DEFAULT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=latin1;
+  `file` text
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `tbl_dokumen`
 --
 
 INSERT INTO `tbl_dokumen` (`id_dokumen`, `id_kategori_dokumen`, `id_user`, `judul`, `keterangan`, `tanggal`, `file`) VALUES
-(12, 3, 37, 'krs', 'ini adalah krs', '1993-12-04', 'krs.pdf'),
-(13, 3, 37, 'khs', 'ini adalah kartu hasil studi', '1993-12-04', 'ipk.pdf'),
-(14, 9, 37, 'buku', 'notulensi rapat hari senin tanggal 12 april 2016\r\n', '1989-12-08', 'transkip.pdf'),
-(15, 3, 37, 'konversi nilai', 'ini adalah form konversi nilai', '1993-12-04', 'Form-Konversi1.pdf');
+(1, 5, 7, 'SOP LAN NO.04.01', 'SOP Penyelenggaraan Sosial, Bimtek dan Advokasi', '2016-02-25', 'SOP-LAN-04_01-PENYELENGGARAN-SOSIALISASI-BIMTEK-DA'),
+(2, 5, 7, 'Pengolahan Media Center dan Permohonan Informasi', 'Pedoman dalam Pelaksanaan Pengelolaan Media Center dan Pelayanan Informasi ', '2016-02-25', 'SOP-LAN-04_04-PENGELOLAAN-MEDIA-CENTER-DAN-PERMOHO'),
+(3, 5, 7, 'Pelayanan HUMAS dan Informasi', 'Pedoman Pelaksanaan Tugas Pengeloaan Data dan Informasi HUMAS', '2016-02-18', 'SOP-PELAYANAN-INFORMASI-DAN-HUBUNGAN-ANTAR-LEMBAGA'),
+(4, 5, 7, 'sop', 'ini adalah SOP', '1993-12-04', 'Form-Konversi.pdf'),
+(5, 3, 7, 'lakip', 'ini lakip', '1993-12-10', 'CFM_LAN_09_PENGELOLAAN_TEKNOLOGI_INFORMASI_(1).pdf'),
+(6, 2, 7, 'notulen', 'ini notulensi', '2016-09-17', '04062014_Notulensi_Rapat_Pengembangan_Web_LAN.docx'),
+(7, 2, 7, 'notulen tes', 'ini notulensi tes', '2016-09-02', '23062014_Notulensi_Rapat_E_office.docx'),
+(8, 3, 7, 'lakip', 'lakip', '2016-09-20', '23062014_Notulensi_Rapat_E_office1.docx');
 
 -- --------------------------------------------------------
 
@@ -99,7 +108,7 @@ INSERT INTO `tbl_dokumen` (`id_dokumen`, `id_kategori_dokumen`, `id_user`, `judu
 CREATE TABLE IF NOT EXISTS `tbl_jabatan` (
   `id_jabatan` int(2) NOT NULL,
   `nama_jabatan` varchar(30) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `tbl_jabatan`
@@ -109,7 +118,8 @@ INSERT INTO `tbl_jabatan` (`id_jabatan`, `nama_jabatan`) VALUES
 (1, 'Kepala Bagian Humas'),
 (2, 'Kepala Sub Bagian Publikasi'),
 (3, 'Kepala Sub Bagian TI'),
-(4, 'Staff');
+(4, 'Staff IT'),
+(5, 'Staff Publikasi');
 
 -- --------------------------------------------------------
 
@@ -139,19 +149,18 @@ INSERT INTO `tbl_kategori` (`id_kategori`, `nama_kategori`, `keterangan`) VALUES
 --
 
 CREATE TABLE IF NOT EXISTS `tbl_kategori_berita` (
-  `id_kategori_berita` int(11) NOT NULL,
-  `slug_kategori_berita` varchar(100) DEFAULT NULL,
-  `nama_ketagori_berita` varchar(100) NOT NULL,
-  `keterangan` text,
-  `urutan` int(11) DEFAULT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+  `id_kategori_berita` int(3) NOT NULL,
+  `nama_kategori` varchar(50) NOT NULL,
+  `keterangan` varchar(100) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `tbl_kategori_berita`
 --
 
-INSERT INTO `tbl_kategori_berita` (`id_kategori_berita`, `slug_kategori_berita`, `nama_ketagori_berita`, `keterangan`, `urutan`) VALUES
-(1, 'ini adalah perjuangan', 'teknologi', NULL, NULL);
+INSERT INTO `tbl_kategori_berita` (`id_kategori_berita`, `nama_kategori`, `keterangan`) VALUES
+(7, 'LAN', 'Berisi Semua Berita yang Berkaitan dengan Kegiatan Seputar LAN'),
+(8, 'Lintas Instansi', 'Berisi Semua Berita yang Berhubungan Dengan Instansi yang berada dibawah LAN');
 
 -- --------------------------------------------------------
 
@@ -187,18 +196,14 @@ CREATE TABLE IF NOT EXISTS `tbl_komentar` (
   `id_pegawai` int(3) NOT NULL,
   `isi_komentar` text NOT NULL,
   `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `tbl_komentar`
 --
 
 INSERT INTO `tbl_komentar` (`id_komentar`, `id_diskusi`, `id_pegawai`, `isi_komentar`, `date`) VALUES
-(16, 3, 1, 'testis', '2016-09-16 12:27:03'),
-(17, 2, 1, 'kampret', '2016-09-16 12:27:11'),
-(18, 2, 1, 'testis', '2016-09-16 12:27:15'),
-(19, 2, 1, 'yuk', '2016-09-16 12:27:19'),
-(20, 2, 1, 'yes', '2016-09-16 12:27:22');
+(1, 3, 6, 'testis', '2016-09-20 09:40:48');
 
 -- --------------------------------------------------------
 
@@ -218,14 +223,19 @@ CREATE TABLE IF NOT EXISTS `tbl_pegawai` (
   `email` varchar(30) NOT NULL,
   `alamat_rumah` varchar(50) NOT NULL,
   `telepon` varchar(12) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `tbl_pegawai`
 --
 
 INSERT INTO `tbl_pegawai` (`id_pegawai`, `nip`, `nama_lengkap`, `tempat_lahir`, `tgl_lahir`, `jenis_kelamin`, `agama`, `id_jabatan`, `email`, `alamat_rumah`, `telepon`) VALUES
-(1, '', 'bedul', '', '0000-00-00', 'laki-laki', 'islam', 1, '', '', '');
+(1, '196609291986022001', 'Hj. Fateha S.IP', 'Bandung', '1986-09-06', 'perempuan', 'islam', 1, 'fateha@gmail.com', 'jalan raya hankam pondok melati', '085693677158'),
+(2, '196609291986022002', 'Budi Prayitno, SIP, M.SI', 'Purwokerto', '1989-12-04', 'laki-laki', 'islam', 2, 'prayitno@gmail.com', 'jalan raya perintis', '085693677158'),
+(3, '196609291986022003', 'Danang Dwi Cahyono, S.os', 'Banyuwangi', '1980-04-05', 'laki-laki', 'islam', 5, 'danang@gmail.com', 'jalan raya menteng ', '085693677158'),
+(5, '196609291986022007', 'Paracoyo Cipto Nugroho, A, Md', 'Bogor', '1979-12-04', 'laki-laki', 'islam', 5, 'pracoyo@gmail.com', 'jalan raya perintis', '085693677158'),
+(6, 'admin', 'azis dzelani', 'bekasi', '1993-12-04', 'laki-laki', 'islam', 4, 'azisdzelani@gmail.com', 'jalan raya hankam', '085693677158'),
+(7, '1111093000034', 'Ari ', 'Purworejo', '1976-12-04', 'laki-laki', 'islam', 3, 'ari@gmail.com', 'jalan raya pendidikan', '085693677158');
 
 -- --------------------------------------------------------
 
@@ -240,22 +250,19 @@ CREATE TABLE IF NOT EXISTS `tbl_user` (
   `username` varchar(30) NOT NULL,
   `password` varchar(64) NOT NULL,
   `status` set('Aktif','Tidak Aktif') NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=47 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `tbl_user`
 --
 
 INSERT INTO `tbl_user` (`id_user`, `id_pegawai`, `level_user`, `username`, `password`, `status`) VALUES
-(37, 39, 'Admin', 'admin', 'admin', 'Aktif'),
-(38, 1, '', '', '', 'Aktif'),
-(39, 2, '', '', '', 'Aktif'),
-(41, 4, 'Kepala Bagian', '1111093000034', '1111093000034', 'Aktif'),
-(42, 5, 'Kepala Bagian', '', '', 'Aktif'),
-(43, 6, 'Kepala Bagian', '', '', 'Aktif'),
-(44, 7, 'Kepala Bagian', '', '', 'Aktif'),
-(45, 8, 'Kepala Bagian', '', '', 'Aktif'),
-(46, 9, 'Kepala Sub Bagian', 'kasubag', 'kasubag', 'Aktif');
+(2, 1, 'Kepala Bagian', '196609291986022001', '196609291986022001', 'Aktif'),
+(3, 2, 'Kepala Sub Bagian', '196609291986022002', '196609291986022002', 'Aktif'),
+(4, 3, 'Staff', '196609291986022003', '196609291986022003', 'Aktif'),
+(6, 5, 'Staff', '196609291986022007', '196609291986022007', 'Aktif'),
+(7, 6, 'Admin', 'admin', 'admin', 'Aktif'),
+(8, 7, 'Kepala Sub Bagian', '1111093000034', '1111093000034', 'Aktif');
 
 --
 -- Indexes for dumped tables
@@ -265,8 +272,7 @@ INSERT INTO `tbl_user` (`id_user`, `id_pegawai`, `level_user`, `username`, `pass
 -- Indexes for table `tbl_berita`
 --
 ALTER TABLE `tbl_berita`
-  ADD PRIMARY KEY (`id_berita`),
-  ADD UNIQUE KEY `slug_berita` (`slug_berita`);
+  ADD PRIMARY KEY (`id_berita`);
 
 --
 -- Indexes for table `tbl_diskusi`
@@ -296,8 +302,7 @@ ALTER TABLE `tbl_kategori`
 -- Indexes for table `tbl_kategori_berita`
 --
 ALTER TABLE `tbl_kategori_berita`
-  ADD PRIMARY KEY (`id_kategori_berita`),
-  ADD UNIQUE KEY `slug_kategori_berita` (`slug_kategori_berita`);
+  ADD PRIMARY KEY (`id_kategori_berita`);
 
 --
 -- Indexes for table `tbl_kategori_dokumen`
@@ -331,7 +336,7 @@ ALTER TABLE `tbl_user`
 -- AUTO_INCREMENT for table `tbl_berita`
 --
 ALTER TABLE `tbl_berita`
-  MODIFY `id_berita` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_berita` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
 --
 -- AUTO_INCREMENT for table `tbl_diskusi`
 --
@@ -341,12 +346,12 @@ ALTER TABLE `tbl_diskusi`
 -- AUTO_INCREMENT for table `tbl_dokumen`
 --
 ALTER TABLE `tbl_dokumen`
-  MODIFY `id_dokumen` int(3) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=16;
+  MODIFY `id_dokumen` int(3) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=9;
 --
 -- AUTO_INCREMENT for table `tbl_jabatan`
 --
 ALTER TABLE `tbl_jabatan`
-  MODIFY `id_jabatan` int(2) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
+  MODIFY `id_jabatan` int(2) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=6;
 --
 -- AUTO_INCREMENT for table `tbl_kategori`
 --
@@ -356,7 +361,7 @@ ALTER TABLE `tbl_kategori`
 -- AUTO_INCREMENT for table `tbl_kategori_berita`
 --
 ALTER TABLE `tbl_kategori_berita`
-  MODIFY `id_kategori_berita` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+  MODIFY `id_kategori_berita` int(3) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=9;
 --
 -- AUTO_INCREMENT for table `tbl_kategori_dokumen`
 --
@@ -366,17 +371,17 @@ ALTER TABLE `tbl_kategori_dokumen`
 -- AUTO_INCREMENT for table `tbl_komentar`
 --
 ALTER TABLE `tbl_komentar`
-  MODIFY `id_komentar` int(3) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=21;
+  MODIFY `id_komentar` int(3) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
 --
 -- AUTO_INCREMENT for table `tbl_pegawai`
 --
 ALTER TABLE `tbl_pegawai`
-  MODIFY `id_pegawai` int(3) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+  MODIFY `id_pegawai` int(3) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=8;
 --
 -- AUTO_INCREMENT for table `tbl_user`
 --
 ALTER TABLE `tbl_user`
-  MODIFY `id_user` int(3) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=47;
+  MODIFY `id_user` int(3) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=9;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
